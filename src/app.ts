c3543d395284f622c9f393cdd12d616311ee9003
HarmanNahal26 <hnahal@academic.rrc.ca>
2026-03-15 13:26:21 -0500

    Added the health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,4 @@ import express, { Express } from "express";
 // Initialize Express application
 const app: Express = express();
 
-// Define a route
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
-
 export default app;
