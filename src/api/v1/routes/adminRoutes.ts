41db3f00bf06c1640efba13e31c71555f12194ae
HarmanNahal26 <hnahal@academic.rrc.ca>
2026-03-15 21:11:20 -0500

    fixing error

--- src/api/v1/routes/adminRoutes.ts
+++ src/api/v1/routes/adminRoutes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { setCustomClaims } from "../controllers/adminController";
 import authenticate from "../middleware/authenticate";
-import isAuthorized from "../middleware/authorize";
+
 
 const router: express.Router = express.Router();
 
